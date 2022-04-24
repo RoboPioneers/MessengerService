bf38c6402a0ac736e35a715f68dbab60e0f19a16
Vincent <rumble.vecent@outlook.com>
2022-04-24 15:59:33 +0800

    Finish basic function.

---
+++ Tester/Launcher.cs
@@ -0,0 +1,33 @@
+﻿using Google.Protobuf;
+using Grpc.Net.Client;
+using Messenger;
+using Messenger.Protocols;
+
+namespace Tester;
+
+public class Launcher
+{
+    public static void Main(string[] arguments)
+    {
+        var channel = GrpcChannel.ForAddress("http://localhost:5000");
+        var client = new Router.RouterClient(channel);
+
+        var sensorRequest = new SensorRequest
+        {
+            Type = "Gyr",
+            ID = 0
+        };
+        
+        var package = new Messenger.Message
+        {
+            ID = 0,
+            Protocol = "SNS_REQ",
+            Content = sensorRequest.ToByteString()
+        };
+
+        client.Write(new WriteRequest
+        {
+            Data = package.ToByteString()
+        });
+    }
+}
